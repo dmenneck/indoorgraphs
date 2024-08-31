d1d16bf520186869158dc730c0d89e3c21c74381
dmenneck <dmennec1@uni-koeln.de>
2024-08-31 14:23:15 +0200

    feat: add webpack and jest support

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    indoorgraphs: path.resolve(__dirname, 'src/index.ts')
+  },
+  output: {
+    publicPath: "",
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: 'ts-loader'
+      }
+    ]
+  }
+}
